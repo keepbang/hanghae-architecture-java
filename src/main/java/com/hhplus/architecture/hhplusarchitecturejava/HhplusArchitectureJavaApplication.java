65be31661acb9d6cd7f2d649ff45364b58a351bd
keepbang@kakao.com <rlqud1125@naver.com>
2024-03-23 17:30:54 +0900

    chore: init project

---
+++ src/main/java/com/hhplus/architecture/hhplusarchitecturejava/HhplusArchitectureJavaApplication.java
@@ -0,0 +1,13 @@
+package com.hhplus.architecture.hhplusarchitecturejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusArchitectureJavaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HhplusArchitectureJavaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/hhplus/architecture/hhplusarchitecturejava/HhplusArchitectureJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhplus.architecture.hhplusarchitecturejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusArchitectureJavaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
